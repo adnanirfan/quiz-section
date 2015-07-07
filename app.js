c23ff2283620ae712e42fbeb6c5f22834760513e
Taimoor Tariq <taimoor_t@yahoo.com>
2015-07-07 02:17:38 -0700

    aap.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 /**
  * Created by Taimoor on 7/7/2015.
  */
+var app = angular.module("app", ["ngMaterial", "ngNewRouter"]);
+
+
+app.controller("quizController", function($router){
+
+    $router.config([
+        {
+            path : "/",
+            component : "home"
+        },
+        {
+            path : "/home",
+            component : "home"
+        }
+
+    ]);
+
+});
